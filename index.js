b78f9a54fdef1f876edbaae315cebd3b377354c4
mgwedd <michael.wedd@gmail.com>
2019-02-24 21:25:50 -0500

    fixed jQuery code in index.js

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-// ADD SHOPPING LIST ITEM EVENT HANDLER
 $(function() {
-    // Take user's added items and append them to the shopping list
     $( "#js-shopping-list-form" ).submit( function(event){
         event.preventDefault();
         
         const itemAdded = $("#shopping-list-entry").val();
 
-        $(".shopping-list").append(`<li><span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button>
-        ${itemAdded}<button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        $(".shopping-list").append(
+            `<li>
+            <span class="shopping-item">${itemAdded}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`);
         
-        // clear the text input field.
         $("#shopping-list-entry").val('');
     });  
-});
 
-// CHECK / UNCHECK ITEM EVENT HANDLER
-$(function() {
     $( ".shopping-item-toggle" ).click( 'button', function(event) {
-        $( "span.shopping-item" ).toggleClass( "shopping-item__checked" );
+        event.preventDefault();
+        $( this ).closest("li").find(".shopping-item").toggleClass( "shopping-item__checked" );
+    });
+
+    $( ".shopping-item-delete" ).click( 'button', function(event) {
+        event.preventDefault();
+        $( this ).parent().closest("li").remove();
     });
 });
-
-// DELETE ITEM EVENT HANDLER
-$(function() {
-    $( ".shopping-item-delete" ).click( 'button', 'li', function(event) {
-        $( this ).remove();
-    });
-});
-
-
