911c2477c6b254ca321e1b4afebc93c8ad3c0afc
mgwedd <michael.wedd@gmail.com>
2019-02-25 20:27:53 -0500

    fixed event delegation for check and delete functions

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 $(function() {
-    $( "#js-shopping-list-form" ).submit( function(event){
-        event.preventDefault();
-        
-        const itemAdded = $("#shopping-list-entry").val();
+  $( "#js-shopping-list-form" ).submit( function(event){
+    event.preventDefault();
+    
+    const itemAdded = $("#shopping-list-entry").val();
 
-        $(".shopping-list").append(
-            `<li>
-            <span class="shopping-item">${itemAdded}</span>
-            <div class="shopping-item-controls">
-              <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-              </button>
-              <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-              </button>
-            </div>
-          </li>`);
-        
-        $("#shopping-list-entry").val('');
-    });  
+    $(".shopping-list").append(
+        `<li>
+        <span class="shopping-item">${itemAdded}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+    
+    $("#shopping-list-entry").val('');
+  });
 
-    $( ".shopping-item-toggle" ).click( 'button', function(event) {
-        event.preventDefault();
-        $( this ).closest("li").find(".shopping-item").toggleClass( "shopping-item__checked" );
-    });
+  $( "ul" ).on("click", ".shopping-item-toggle", function(event) {
+    event.preventDefault();
+    $( this ).closest("li").find(".shopping-item").toggleClass( "shopping-item__checked" );
+  });
 
-    $( ".shopping-item-delete" ).click( 'button', function(event) {
-        event.preventDefault();
-        $( this ).parent().closest("li").remove();
-    });
+  $( "ul" ).on( "click", ".shopping-item-delete", function(event) {
+    event.preventDefault();
+    $( this ).parent().closest("li").remove();
+  });
 });
